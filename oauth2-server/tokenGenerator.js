7b94b5bda3c2105220768971eaf4fde5adbe1a8d
Alvaro Ferreira Costa <aferrcos@emeal.nttdata.com>
2022-12-14 22:13:10 -0300

    Adding exception handling

--- oauth2-server/tokenGenerator.js
+++ oauth2-server/tokenGenerator.js
@@ -3,26 +3,30 @@ const clientSchema = require('./clientModel');
 const { compare } = require('bcrypt');
 
 exports.TokenGenerator = async (request, response) => {
-  const client = request.body
-  const findClient = await clientSchema.findOne({client_id: client.client_id});
-  console.log(findClient)
-  if(!findClient) {
-    return res.send("Cliente não existe");
-  }
+    try {
+      const client = request.body
+      const findClient = await clientSchema.findOne({client_id: client.client_id});
+      if(!findClient) {
+        throw new Error("Client ID does not exists!");
+      }
+    
+      const secretMatches = await compare(client.client_secret, findClient.client_secret);
+      if(!secretMatches) {
+        throw new Error("Invalid Client Secret");
+      }
+    
+      const token = sign({client}, "019acc25a4e242bb55ad489832ada12d", {
+        subject: findClient.name,
+        expiresIn: "900s"
+      });
+    
+      return response.json({
+        access_token: token,
+        expires_in: 900,
+        type: "Bearer"
+      });
 
-  const secretMatches = await compare(client.client_secret, findClient.client_secret);
-  if(!secretMatches) {
-    res.send("Chave secreta do cliente é inválida")
-  }
-
-  const token = sign({client}, "019acc25a4e242bb55ad489832ada12d", {
-    subject: findClient.name,
-    expiresIn: "1d"
-  });
-
-  return response.json({
-    access_token: token,
-    expires_in: "1 Dia",
-    type: "Bearer"
-  });
+    }catch (error) {
+      return response.status(400).send(error.message);
+    }
 }
